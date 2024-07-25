6db7eb09181456e31e9d7727ef95da065f83265b
Yeonnnny <kdoy030@gmail.com>
2024-07-25 13:20:49 +0900

    240725  게시판 목록 관련 repositiory 추가 (카테고리, 검색어, 페이지 반영)

--- src/main/java/com/example/community/repository/JobBoardRepository.java
+++ src/main/java/com/example/community/repository/JobBoardRepository.java
@@ -2,6 +2,8 @@ package com.example.community.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +13,17 @@ import com.example.community.entity.JobBoardEntity;
 
 public interface JobBoardRepository extends JpaRepository<JobBoardEntity, Long>{
     
-    // 카테고리에 해당하는 게시글 리스트 반환 (최신순)
     @Query("SELECT j FROM JobBoardEntity j WHERE " +
-                        "j.category =: category " +
-                        "ORDER BY j.createDate DESC")
+    "j.category =: category AND " +
+    "j.reported = false "+
+    "ORDER BY j.createDate DESC")
     List<JobBoardEntity> findByCategoryOrderByCreateDateDesc(@Param("category") BoardCategory category);
+    
+    // 카테고리에 해당하는 (신고당하지 않은) 게시글 리스트 반환 (최신순)
+    @Query("SELECT j FROM JobBoardEntity j WHERE " +
+    "j.category = :category AND " +
+    "j.reported = false AND " +
+    "LOWER(j.title) LIKE LOWER(CONCAT('%', :searchWord, '%'))")
+    Page<JobBoardEntity> findByCategoryTitleContainingAndReportedIsFalse(@Param("category")BoardCategory category, 
+        @Param("searchWord") String searchWord, Pageable pageRequest);
 }
