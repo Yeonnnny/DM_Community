a297e21d74e77244ea530a1187084549d9b12411
Yeonnnny <kdoy030@gmail.com>
2024-07-30 17:41:42 +0900

    240730 BoardEntity와 JobBoardEntity를 BoardListDTO로 변환하는 함수 생성

--- src/main/java/com/example/community/dto/display/BoardListDTO.java
+++ src/main/java/com/example/community/dto/display/BoardListDTO.java
@@ -2,6 +2,9 @@ package com.example.community.dto.display;
 
 import java.time.LocalDateTime;
 
+import com.example.community.entity.BoardEntity;
+import com.example.community.entity.JobBoardEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,4 +30,46 @@ public class BoardListDTO { // 게시물 목록 화면에 필요한 속성들만
     private LocalDateTime deadline;
     private int limitNumber;
     private int currentNumber;
+
+    /**
+     * BoardEntity를 BoardListDTO로 변환하는 함수 (deadline : null, limitNumber : 0, currentNumber : 0)
+     * @param entity
+     * @return
+     */
+    public static BoardListDTO convertToDTO (BoardEntity entity){
+        return BoardListDTO.builder()
+                .boardId(entity.getBoardId())
+                .memberId(entity.getMemberEntity().getMemberId())
+                .memberGroup(entity.getMemberGroup())
+                .title(entity.getTitle())
+                .hitCount(entity.getHitCount())
+                .likeCount(entity.getLikeCount())
+                .createDate(entity.getCreateDate())
+                .deadline(null)
+                .limitNumber(0)
+                .currentNumber(0)
+                .build();
+    }
+
+    /**
+     * JobBoardEntity를 BoardListDTO로 변환하는 함수
+     * @param entity
+     * @return
+     */
+    public static BoardListDTO convertToDTO (JobBoardEntity entity){
+        return BoardListDTO.builder()
+                .boardId(entity.getBoardId())
+                .memberId(entity.getBoardEntity().getMemberEntity().getMemberId())
+                .memberGroup(entity.getBoardEntity().getMemberGroup())
+                .title(entity.getBoardEntity().getTitle())
+                .hitCount(entity.getBoardEntity().getHitCount())
+                .likeCount(entity.getBoardEntity().getLikeCount())
+                .createDate(entity.getBoardEntity().getCreateDate())
+                .deadline(entity.getDeadline())
+                .limitNumber(entity.getLimitNumber())
+                .currentNumber(entity.getCurrentNumber())
+                .build();
+    }
+
+
 }
