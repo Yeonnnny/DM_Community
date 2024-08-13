b7e859d42a7f0a578ebd2247143ec2ddc5ccb2da
Yeonnnny <kdoy030@gmail.com>
2024-08-13 11:32:03 +0900

    240813 공백 정렬

--- src/main/java/com/example/community/repository/JobBoardRepository.java
+++ src/main/java/com/example/community/repository/JobBoardRepository.java
@@ -13,28 +13,28 @@ import com.example.community.entity.JobBoardEntity;
 
 public interface JobBoardRepository extends JpaRepository<JobBoardEntity, Long>{
 
-    // 카테고리에 해당하는 (신고당하지 않은) JobBoardEntities 반환 (최신순)
-    @Query("SELECT j FROM JobBoardEntity j " +
-            "JOIN j.boardEntity b " +
-            "WHERE b.category = :category " +
-            "AND LOWER(b.title) LIKE LOWER(CONCAT('%', :searchWord, '%')) " +
-            "AND b.reported = false " +
-            "ORDER BY b.createDate DESC")
-    Page<JobBoardEntity> findByCategoryAndTitleContainingAndReportedIsFalse(@Param("category") BoardCategory category, 
-                                                                            @Param("searchWord") String searchWord, 
-                                                                            Pageable pageable); 
-    
-    // 카테고리에 해당하는 (신고당하지 않은) BoardListDTOs 반환 (최신순)
-    @Query("SELECT new com.example.community.dto.display.BoardListDTO(b.boardId, b.memberEntity.memberId, b.memberGroup, b.title, b.hitCount, b.likeCount, b.createDate, j.deadline, j.limitNumber, j.currentNumber) " +
-            "FROM JobBoardEntity j " +
-            "JOIN j.boardEntity b " +
-            "WHERE b.category = :category " +
-            "AND LOWER(b.title) LIKE LOWER(CONCAT('%', :searchWord, '%')) " +
-            "AND b.reported = false " +
-            "ORDER BY b.createDate DESC")
-    Page<BoardListDTO> findBoardListByCategoryAndTitleContainingAndReportedIsFalse(@Param("category") BoardCategory category, 
-                                                                                    @Param("searchWord") String searchWord, 
-                                                                                    Pageable pageable);
+        // 카테고리에 해당하는 (신고당하지 않은) JobBoardEntities 반환 (최신순)
+        @Query("SELECT j FROM JobBoardEntity j " +
+                "JOIN j.boardEntity b " +
+                "WHERE b.category = :category " +
+                "AND LOWER(b.title) LIKE LOWER(CONCAT('%', :searchWord, '%')) " +
+                "AND b.reported = false " +
+                "ORDER BY b.createDate DESC")
+        Page<JobBoardEntity> findByCategoryAndTitleContainingAndReportedIsFalse(@Param("category") BoardCategory category, 
+                                                                                @Param("searchWord") String searchWord, 
+                                                                                Pageable pageable); 
 
-    
+        // 카테고리에 해당하는 (신고당하지 않은) BoardListDTOs 반환 (최신순)
+        @Query("SELECT new com.example.community.dto.display.BoardListDTO(b.boardId, b.memberEntity.memberId, b.memberGroup, b.title, b.hitCount, b.likeCount, b.createDate, j.deadline, j.limitNumber, j.currentNumber) " +
+                "FROM JobBoardEntity j " +
+                "JOIN j.boardEntity b " +
+                "WHERE b.category = :category " +
+                "AND LOWER(b.title) LIKE LOWER(CONCAT('%', :searchWord, '%')) " +
+                "AND b.reported = false " +
+                "ORDER BY b.createDate DESC")
+        Page<BoardListDTO> findBoardListByCategoryAndTitleContainingAndReportedIsFalse(@Param("category") BoardCategory category, 
+                                                                                        @Param("searchWord") String searchWord, 
+                                                                                        Pageable pageable);
+
+
 }
