93fc5df0730934c90f6776b9fa0b3c9ffda1f26c
Yeonnnny <kdoy030@gmail.com>
2024-08-15 21:24:09 +0900

    240815 댓글 목록 요청에 대한 로직 구현

--- src/main/java/com/example/community/controller/ReplyController.java
+++ src/main/java/com/example/community/controller/ReplyController.java
@@ -1,13 +1,21 @@
 package com.example.community.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.example.community.dto.ReplyDTO;
 import com.example.community.service.ReplyService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
@@ -25,5 +33,32 @@ public class ReplyController {
     public long getReplyCount(@RequestParam(name = "boardId")Long boarId) {
         return replyService.getBoardReplyCount(boarId);
     }
+
+    // ================== 댓글 목록 ==================
+    // list로 보내지 않고 RequestMapping으로 보내야겠다 ㅎㅎ 
+    // 원래 html을 보면서 같이 해야 하는데.. 지금 프로젝트 실행이 안되므로 .... 허허
+    // 일단 감으로 해보겠습니다.
+
+
+    /**
+     * ajax - 게시글에 대한 댓글 목록 요청
+     * @param boardId
+     * @param memberId
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/reply/getList", method=RequestMethod.GET)
+    public String getList(@RequestParam(name = "boardId") Long boardId, @RequestParam(name = "memberId") String memberId, Model model) {
+        
+        List<ReplyDTO> replyDTOs = new ArrayList<>();
+        
+        replyDTOs = replyService.getList(boardId, memberId);
+        
+        model.addAttribute("list", replyDTOs);
+        
+        return "/board/detail::#result";
+    }
+    
+
     
 }
--- src/main/java/com/example/community/repository/ReplyRepository.java
+++ src/main/java/com/example/community/repository/ReplyRepository.java
@@ -1,5 +1,7 @@
 package com.example.community.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,9 @@ public interface ReplyRepository extends JpaRepository<ReplyEntity,Long>{
     @Query("SELECT COUNT(r) FROM ReplyEntity r WHERE r.boardEntity = :boardEntity")
     long countByBoardEntity(@Param("boardEntity") BoardEntity boardEntity);
 
+    // boardEntity에 해당하는 댓글들을 반환하는 메서드
+    @Query("SELECT r FROM ReplyEntity r " +
+            "WHERE r.boardEntity =: boardEntity " +
+            "ORDER BY r.createDate DESC")
+    List<ReplyEntity> findByBoardEntity(@Param("boardEntity") BoardEntity boardEntity);
 }
